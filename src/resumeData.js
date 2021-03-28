7fb60ecded1a508b157394c6ea9d502f589425d7
mathew <mnalbete@uw.edu>
2021-03-27 21:57:03 -0700

    react portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,92 @@
+let resumeData = {
+  "imagebaseurl": "https://github.com/mnalbete",
+  "name": "Mathew Albete",
+  "role": "Full stack developer",
+  "linkedinId": "Your LinkedIn Id",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/mathew-albete-4109ba198/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/mnalbete",
+      "className": "fa fa-github"
+    },
+  ],
+  "aboutme": "I currently am a lead cook at the University of Washington working on become a full stack developer. My reasons to become a full stack developer is that when starting this course I had no idea what to expect wether I would enjoy the course or hate it. But I can say that I really enjoy the course. Although the course toward the end is tough and when I mean tough I mean tough I still try and try but never give up. Even in failer will I succeed. For me that makes me want to do this is the fact of creating something is for me the best thing and the best reword.",
+  "address": "Seattle",
+  "website": "https://github.com/mnalbete",
+  "education": [
+    {
+      "UniversityName": "Le Cordon Bleu",
+      "specialization": "Food and Science",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2017",
+      "Achievements": "Assosiates in Food Science"
+    },
+    {
+      "UniversityName": "Some University",
+      "specialization": "Nutrition",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2019",
+      "Achievements": "Certificate"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "University of Washington",
+      "specialization": "Lead Cook",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      
+    },
+  
+  ],
+  "skillsDescription": "Your skills level",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    
+  ],
+  "portfolio": [
+    {
+      "name": "RecipeNow",
+      "description": "A website where you can share recipes with eachother and creat profiles that go with them",
+      "imgurl": "../images/recipeNow.jpg"
+    },
+    {
+      "name": "inspectNow",
+      "description": "business application where you can inspect a property",
+      "imgurl": "../images/inspectNow.png"
+    },
+    // {
+    //   "name": "",
+    //   "description": "mobileapp",
+    //   "imgurl": "images/portfolio/project2.png"
+    // },
+    // {
+    //   "name": "project4",
+    //   "description": "mobileapp",
+    //   "imgurl": "images/portfolio/phone.jpg"
+    // }
+  ],
+  "testimonials": [
+    {
+      "description": "“Success is the ability to go from failure to failure without losing your enthusiasm.”",
+      "name": "Winston Churchill"
+    },
+    {
+      "description": "“It is fine to celebrate success, but it is more important to heed the lessons of failure.”",
+      "name": "Bill Gates"
+    }
+  ]
+}
+
+export default resumeData
